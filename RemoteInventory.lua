e105d7e2ccb32e90968287e924e04fc6f1954059
JacobHuber <xjacobhuber@gmail.com>
2023-04-16 15:08:23 -0600

    Add mouse handling and modem calls

--- RemoteInventory.lua
+++ RemoteInventory.lua
@@ -18,19 +18,23 @@ local cols = {
 }
 
 function storeThirdRow()
-
+  modem.transmit(port, 0, "storeThirdRow")
 end
 
 function listItems()
-
+  modem.transmit(port, 0, "listItems")
 end
 
 local options = {
   {
+    ["x"] = 1,
+    ["y"] = 1,
     ["text"] = "Store Third Row",
     ["callback"] = storeThirdRow
   },
   {
+    ["x"] = 1,
+    ["y"] = 3,
     ["text"] = "List Items",
     ["callback"] = listItems
   }
@@ -38,26 +42,40 @@ local options = {
 
 
 function setupScreenVars()
-  term.setBackgroundColour(cols["bg"])
-  term.setTextColour(cols["text"])
+  term.setBackgroundColour(cols.bg)
+  term.setTextColour(cols.text)
 end
 
-function drawScreen()
+function drawMenus()
   term.clear()
-  local x, y = { 2, 2 }
+  local x, y = 2, 2
   term.setCursorPos(x, y)
 
   local n = table.getn(options)
   for i = 1, n, 1 do
-    local text = options[i]["text"]
+    local option = options[i]
+    term.setCursorPos(option.x, option.y)
+    local text = option.text
     local len = #text
-    term.blit(text, string.rep("0", len), string.rep("2", len))
-    y = y + 2
-    term.setCursorPos(x, y)
+    term.blit(text, string.rep("0", len), string.rep("2", len))   
+  end
+end
+
+function handleMouseClick()
+  local mouse = os.pullEvent("mouse_click")
+  local n = table.getn(options)
+  for i = 1, n, 1 do
+    local option = options[i]
+    if (mouse.y == option.y) then
+      if mouse.x >= option.x and mouse.x < option.x + #option.text then
+        option.callback()
+      end
+    end
   end
 end
 
 setupScreenVars()
+drawMenus()
 while true do
-  drawScreen()
+  handleMouseClick()
 end
